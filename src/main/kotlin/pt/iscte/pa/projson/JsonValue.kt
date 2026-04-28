cd07851f49b6b4c58de74f137f3aa845dcb25dcb
Andre3648 <alsca1@iscte-iul.pt>
2026-04-28 19:34:42 +0100

    adicionar funcao auxiliar para as proximas classes

--- src/main/kotlin/pt/iscte/pa/projson/JsonValue.kt
+++ src/main/kotlin/pt/iscte/pa/projson/JsonValue.kt
@@ -3,3 +3,9 @@ package pt.iscte.pa.projson
 sealed class JsonValue {
     abstract override fun toString(): String
 }
+
+internal fun toJsonValue(value: Any?): JsonValue = when(value){
+    is JsonValue -> value
+    null, is String, is Number, is Boolean -> JsonPrimitive(value)
+    else -> throw IllegalArgumentException("${value.javaClass.name} is not a simple JsonValue")
+}
